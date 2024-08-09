cc17d9d617f81b50846b8017315cbf563e98f366
Ruperth <97520685+iRuperth@users.noreply.github.com>
2024-08-09 09:10:37 +0000

    actualizacion jumbotron desde home

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,27 +1,34 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
 
 export const Home = () => {
     const { store, actions } = useContext(Context);
-// esto para que al presionar la pestaña, nos lleve a una pagina LINEA 12
+
+    useEffect(() => {
+        // Inicializa el carrusel y establece el intervalo en 5000 ms (5 segundos)
+        const carouselElement = document.querySelector('#carouselExampleFade');
+        if (carouselElement) {
+            const carousel = new bootstrap.Carousel(carouselElement, {
+                interval: 5000, // Cambia de diapositiva cada 5 segundos
+                wrap: true, // Permite que el carrusel vuelva al principio al final
+                ride: 'carousel' // Inicia el carrusel automáticamente
+            });
+        }
+    }, []);
+
     return (
         <div id="carouselExampleFade" className="carousel slide carousel-fade">
             <div className="carousel-inner">
-                <div className="carousel-item active">    
-                    <a href="https://example.com/page1" target="_blank" rel="noopener noreferrer"> 
-                        <img src="https://images.unsplash.com/photo-1531482615713-2afd69097998?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="d-block w-100" alt="First Slide" />
-                    </a>
+                <div className="carousel-item active">
+                
+                    <img src="https://img.freepik.com/foto-gratis/aula-virtual-espacio-estudio_23-2149178706.jpg?t=st=1723192613~exp=1723196213~hmac=5a8c53b98fc8081ce0f78826031202b016eb1128019beb8e54b2f9508617f6aa&w=1380" className="d-block w-100" alt="First Slide" />
                 </div>
                 <div className="carousel-item">
-                    <a href="https://example.com/page2" target="_blank" rel="noopener noreferrer">
-                        <img src="https://via.placeholder.com/800x400.png?text=Second+Slide" className="d-block w-100" alt="Second Slide" />
-                    </a>
+                    <img src="https://img.freepik.com/foto-gratis/mujer-negocios-ordenador-portatil-mano-esta-feliz-exito-retrato-mujer-gafas-blusa-rayas-gritando-entusiasmo-haciendo-gesto-ganador_197531-13438.jpg?t=st=1723192685~exp=1723196285~hmac=7faa32b9dc3b55e7e55660b571c612b19388cde64dac483baf3791d4a6ec970e&w=1380" className="d-block w-100" alt="Second Slide" />
                 </div>
                 <div className="carousel-item">
-                    <a href="https://example.com/page3" target="_blank" rel="noopener noreferrer">
-                        <img src="https://via.placeholder.com/800x400.png?text=Third+Slide" className="d-block w-100" alt="Third Slide" />
-                    </a>
+                    <img src="https://img.freepik.com/foto-gratis/jovencitas-que-miran-ordenador-portatil-junto_23-2147655843.jpg?t=st=1723192975~exp=1723196575~hmac=ef90509bdffa2f4885fa72349a79644ea59d21ad210bb665780a5cc265828351&w=1380" className="d-block w-100" alt="Third Slide" />
                 </div>
             </div>
             <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
