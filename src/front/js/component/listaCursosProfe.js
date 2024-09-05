4cbcdd43d31b6b2b673b8fa5b0931c24183b6569
Javier Ramos <162994894+JavierRamos1988@users.noreply.github.com>
2024-09-05 21:23:33 +0000

    papas

--- src/front/js/component/listaCursosProfe.js
+++ src/front/js/component/listaCursosProfe.js
@@ -23,7 +23,7 @@ const ListaCursosProfe = ({ cursos }) => {
                             <h3 className="cardProfe__title">{curso.title}</h3>
                         </div>
                         <div className="cardProfe__content">
-                            <p className="cardProfe__matriculas">Nº Alumnos: {curso.matriculas.length}</p>
+                            <p className="cardProfe__matriculas">{curso.matriculas ? curso.matriculas.length : 0} matriculados</p>
                         </div>
                     </div>
                 </div>
