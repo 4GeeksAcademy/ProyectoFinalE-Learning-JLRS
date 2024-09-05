dbe1957d2c0e5491f4cb3abb376e065162ba5055
Javier Ramos <162994894+JavierRamos1988@users.noreply.github.com>
2024-09-05 16:55:00 +0000

    sincro lois

--- src/front/js/pages/perfil.js
+++ src/front/js/pages/perfil.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect} from "react";
 import { Context } from "../store/appContext";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../../styles/perfil.css";
@@ -6,18 +6,91 @@ import { Row } from 'react-bootstrap';
 
 function Perfil() {
   const { store, actions } = useContext(Context);
+  const [file, setFile] = useState();
+  const [uploadedUrl, setUploadedUrl] = useState('');
+
+  const [dataForm, setDataForm] = useState({
+      name: '',
+      lastname: '',
+      telefono: '',
+      address: '',
+      avatar: '',
+      precio: '',
+  })
+
+  // Maneja los cambios en los campos de texto y en el archivo
+  const handleChange = (e) => {
+      const { name, value, type, files } = e.target;
+      if (type === 'file') {
+          // Si el tipo de campo es 'file', actualiza el estado con el archivo seleccionado
+          setDataForm({ ...dataForm, [name]: files[0] });
+      } else {
+          // Para campos de texto, simplemente actualiza el estado con el valor ingresado
+          setDataForm({ ...dataForm, [name]: value });
+      }
+  };
+
+  useEffect(() => {
+      handleUpload()
+  }, [file])
+
+
+  const handleFileChange = (e) => {
+      setFile(e.target.files[0]);
+  };
+
+  const handleUpload = async () => {
+      console.log('-------------------------------------------')
+      const formData = new FormData();
+      formData.append('file', file);
+
+      const response = await fetch(`${process.env.BACKEND_URL}/api/upload`, {
+
+          method: 'POST',
+          body: formData,
+      });
+
+      const data = await response.json();
+      console.log(data)
+      if (data.secure_url) {
+          setUploadedUrl(data.secure_url);
+      }
+  };
+  const handleSubmit = async (e) => {
+      e.preventDefault()
+      // Crear un objeto FormData para manejar archivos e enviar datos al servidor, incluidos archivos.
+      // const formData = new FormData();
+      // Object.keys(dataForm).forEach(key => { //Object.keys(dataForm) obtiene un array con los nombres de los campos del formulario
+      //     formData.append(key, dataForm[key]); // añade cada par clave-valor al objeto, por ejemplo formData.append('title', 'Curso de React');
+      // });
+      dataForm.portada = uploadedUrl
+      const resp = await actions.crearCurso(dataForm);
+
+      console.log(dataForm)
+      if (resp.success) modalToggle()
+      navigate('/vistaProfe')
+  }
+  
   return (
-    <div className="container overflow-hidden text-center containerPrincipal">
+    <div className="text-center containerPrincipalcontainer d-flex flex-column align-items-center mt-5 p-3">
       <div className="row gx-3" >
-        <div className="col p-3 confIzq">
-            <div className="Pagos">
-              <h3><span className="fa-solid fa-credit-card"></span>Pagos recibidos</h3>
+        <form className="formularioCurso" onSubmit={handleSubmit}>
+          <div className='nombrePersona'> <h1><b>{store.user?.profesor.name} {store.user?.profesor?.lastname}</b></h1></div>
+          <div className='nombrePersona'> <h4> <b>Cambia tu foto y edita tu información de perfil.</b></h4></div>
+          <label>Portada
+            <input type="file" onChange={handleFileChange} />
+          </label>
+          <div className="container">
+            <b>Nombre</b>
+            <div className='firstName d-flex justify-content-center'>
+              <h3>{store.user?.profesor?.name}</h3>
+              <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
             </div>
-        </div>
+          </div>
+        </form>
         <div className="col">
           <div className="p-9 divEditarPerfil">
-            <div className='nombrePersona'> <h2>{store.user?.profesor.name} {store.user?.profesor?.lastname}</h2></div>
-            <div className='nombrePersona'> <h4> <b>Cambia tu foto y edita tu información de perfil.</b></h4></div>
+            
             <div className="container px-4 text-center">
               <div className="row gx-5">
                 <div className="col">
@@ -31,14 +104,7 @@ function Perfil() {
                 </div>
               </div>
             </div>
-            <div className="container">
-              <b>Nombre</b>
-              <div className='firstName d-flex justify-content-center'>
-                {store.user?.profesor?.name}
-
-                <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
-              </div>
-            </div>
+            
             <div className='firstName'>
               <b>Apellidos</b>
               <input
